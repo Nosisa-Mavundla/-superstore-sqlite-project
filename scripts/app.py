39abe69dd809c72c0c03a14fe834e5040ca2b49f
Nosisa <nosisamavundla050@gmail.com>
2026-06-30 17:51:50 +0200

    Update application

--- scripts/app.py
+++ scripts/app.py
@@ -151,6 +151,105 @@ fig5 = px.bar(
 fig5.update_traces(
     hovertemplate="Sales:$%{y:,.2f}<br>Region: %{x}<extra></extra>"
 )
+#Sales by sub-category
+query = """
+SELECT "Sub-Category", SUM(Sales)AS Total_Sales
+FROM superstore
+GROUP BY "Sub-Category"
+ORDER BY Total_Sales DESC
+"""
+df_subcategory = pd.read_sql_query(query, conn)
+fig6= px.bar(
+   df_subcategory,
+   x="Total_Sales",
+   y= "Sub-Category",
+   orientation= "h",
+   title= "Sales by Sub-Category"
+)
+fig6.update_layout(
+   yaxis = {"categoryorder": "total ascending"}
+)
+fig6.update_traces(
+    hovertemplate="Sub-Category: %{y}<br>Sales:$ %{x:,.2f}<extra></extra>"
+
+)
+
+#Top 10 high performing products
+query="""
+
+SELECT "Product Name", SUM(Sales)AS Total_Sales
+FROM superstore
+GROUP BY "Product Name"
+ORDER BY Total_Sales DESC
+LIMIT 10
+"""
+df_productname= pd.read_sql_query(query, conn)
+
+fig7 = px.bar(
+   df_productname,
+   x = "Total_Sales",
+   y = "Product Name",
+   orientation= "h",
+   
+   title= "Top 10 high perfroming products"
+)
+fig7.update_layout(
+   yaxis = {"categoryorder": "total ascending"}
+)
+fig7.update_traces(
+    hovertemplate="Product Name: %{y}<br>Sales:$ %{x:,.2f}<extra></extra>"
+)
+
+#Sales by segment
+query = """
+SELECT Segment, SUM(Sales) AS Total_Sales
+FROM superstore
+GROUP BY Segment
+ORDER BY Total_Sales DESC
+"""
+df_segment= pd.read_sql_query(query, conn)
+
+fig8 = px.treemap(
+   df_segment,
+   path=["Segment"],
+   values="Total_Sales",
+   title="Sales by Segment"
+)
+fig8.update_traces(
+    hovertemplate="Segment: %{label}<br>Sales:$ %{value:,.2f}<br> Share: %{percentRoot:.1%}<extra></extra>"
+)
+fig8.update_traces(
+   textinfo= "label+percent root"
+)
+
+
+
+
+#Top 10 high performing cities
+query = """
+SELECT City, SUM(Sales) AS Total_Sales
+FROM superstore
+GROUP BY City
+ORDER BY Total_Sales DESC
+LIMIT 10
+"""
+
+df_city = pd.read_sql_query(query, conn)
+fig9= px.bar(
+   df_city,
+   x="Total_Sales",
+   y="City",
+   orientation="h",
+   title="Top 10 high performing cities"
+)
+fig9.update_layout(
+   yaxis={"categoryorder" : "total ascending"}
+)
+fig9.update_traces(
+    hovertemplate="City: %{y}<br>Sales:$ %{x:,.2f}<extra></extra>"
+)
+
+
 
 if page == "Executive Summary":
     st.title("Executive Summary")
@@ -195,7 +294,23 @@ elif page == "Business Performance":
       col1, col2, col3 = st.columns(3)
 
       col1.plotly_chart(fig2, use_container_width=True)
-  #Finish up with salesby sub categoryan salesby 10 top performing proucts/write code for both fromscratch
+      col2.plotly_chart(fig6, use_container_width=True)
+      col3.plotly_chart(fig7, use_container_width=True)
+
+    st.subheader("Customer and Market Performance")
+
+    ci_container = st.container()
+
+    with ci_container:
+       col1, col2 =st.columns(2)
+
+       col1.plotly_chart(fig8, use_container_width=True)
+       col2.plotly_chart(fig9, use_container_width=True)
+
+
+       #sales by segment
+       #top 10 performing cities
+
 
 
 
